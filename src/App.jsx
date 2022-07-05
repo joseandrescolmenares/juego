806f31e4a21d39cc53e7036cc1205a0f1c738213
jose <joseandrescolmenares02@gmail.com>
2022-07-05 12:42:16 -0300

    avan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import preguntas from "./Preguntas.js";
+import {useState, useEffect} from "react"
 
 function App() {
+
+  const [preguntas, setPreguntas] = useState(0)
+  const [puntuacion, setPuntuacion] = useState(0)
+  const [final, setFinal] = useState(false)
+
+
   return (
     <main className="app">
-      <div className="lado izquierdo">
-        <div className="numero-Preguntas">
-          <span>Prewgunta 1</span>/ 5
+      <div className="lado-izquierdo">
+        <div className="numero-Pregunta">
+          <span>Pregunta {preguntas}</span> de 5
         </div>
-        <div className="titulo-pregunta">como te llamas</div>
+        <div className="titulo-pregunta">Como te llamas?</div>
       </div>
       <div className="lado-derecho">
         <button>opcion 1</button>
