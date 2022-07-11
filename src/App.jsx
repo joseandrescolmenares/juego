89dad69d499935ebc105210bdc0b90330122345e
jose <joseandrescolmenares02@gmail.com>
2022-07-11 15:53:46 -0300

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import Juego from './components/Juego'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Juego from "./components/Juego";
+import Landing from "./components/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
- return (
-  <>
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}/>
-        {/* <Route path="expenses" element={<Expenses />} /> */}
-        <Route path="/juego" element={ <Juego  />} />
-      </Routes>
-    
-  </BrowserRouter>
- 
-   
-  </>
- )
-}
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/juego" element={<Juego />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from './css/Landing.module.css'
+
+const Landing = () => {
+return ( 
+ <>
+    
+    <div className={s.card}>
+        <div className={s.div}>
+            <h1>Bienvenidos a mi Juego</h1>
+            z
+        </div>
+    </div>
+    
+ </>
+)
+}
+
+
+export default Landing
